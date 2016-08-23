41d82a5791a6bea6d2acb74defa2108b46af351c
WangRuqiang <likegun94@gmail.com>
2016-08-23 11:36:58 +0800

    改版

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 'use strict';
 var format = require('util').format;
 
+function isOption(options, name, optionStartWith) {
+  return Boolean(name[0] === optionStartWith && options[name] !== undefined);
+}
+
 /**
  * 根据options解析argv数组
  * @param {Array} argv process.argv
@@ -10,36 +14,42 @@ var format = require('util').format;
  */
 module.exports = function(argv, options, optionStartWith) {
   var ret = {};
-  var currentOption;
   optionStartWith = optionStartWith || '-';
-  for(var index = 0; index < argv.length; index ++) {
-    if(argv[index][0] === optionStartWith) {
-      currentOption = argv[index];
-      if(options[currentOption] !== undefined) {
-        ret[currentOption] = [];
-        switch (typeof options[currentOption]) {
-          case 'string':
-            if(options[currentOption] !== 'multiple') {
-              throw new Error(format('invalid option count string [%s], can only accept string "multiple"', options[currentOption]));
-            }
-            break;
-          case 'number':
-            var parameterCount = options[currentOption];
-            for(var count = 1; count <= parameterCount; count++) {
-              ret[currentOption].push(argv[++index]);
-            }
-            currentOption = null;
-            break;
-          default:
-            throw new Error(format('invalid option count [%s] type [%s], can only accept number and string "multiple"', options[currentOption],typeof options[currentOption]));
+  for(var i = 0; i < argv.length; i ++) {
+    if(isOption(options, argv[i], optionStartWith)) {
+      var optionName = argv[i];
+      ret[optionName] = [];
+      for(var j = i + 1; j < argv.length; j++) {
+        if(isOption(options, argv[j], optionStartWith))
+          break;
+        else {
+          ret[optionName].push(argv[j]);
+          i++;
         }
       }
-      else {
-        throw new Error(format('invalid option [%s]', argv[index]));
-      }
     }
-    else if(currentOption){
-      ret[currentOption].push(argv[index]);
+    else {
+      throw new Error(format('invalid option [%s]', argv[i]));
+    }
+  }
+  for(var key in ret) {
+    switch (parseInt(options[key])) {
+      case 0:
+        if(ret[key].length !== 0) {
+          throw new Error(format('option [%s] should not have any arguments,but get %s.', key, ret[key].toString()));
+        }
+        ret[key] = true;
+        break;
+      case 1:
+        if(ret[key].length !== 1) {
+          throw new Error(format('option [%s] must have only 1 argument,but get %s.', key, ret[key].toString()));
+        }
+        ret[key] = ret[key][0];
+        break;
+      default:
+        if(ret[key].length !== parseInt(options[key])) {
+          throw new Error(format('option [%s] must have %d arguments,but get %s.', key, parseInt(options[key]), ret[key].toString()));
+        }
     }
   }
   return ret;
